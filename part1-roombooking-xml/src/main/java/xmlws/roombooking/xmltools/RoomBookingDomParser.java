c15f7b02c89250383efa9950e1f41858a86cd45a
matthieulemoine34 <matthieu.lemoine@iut-rodez.fr>
2020-02-26 09:30:42 +0100

    debut analyse des classe tests

---
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
@@ -0,0 +1,10 @@
+package xmlws.roombooking.xmltools;
+
+import java.io.InputStream;
+
+public class RoomBookingDomParser implements RoomBookingParser {
+    @Override
+    public RoomBooking parse(InputStream inputStream) {
+        return null;
+    }
+}
---
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingSaxParser.java
@@ -0,0 +1,12 @@
+package xmlws.roombooking.xmltools;
+
+import java.io.InputStream;
+
+public class RoomBookingSaxParser implements RoomBookingParser {
+
+
+    @Override
+    public RoomBooking parse(InputStream inputStream) {
+        return null;
+    }
+}
